89bee1c301839ea2d32ff08b1ed27e41fd86be31
Jason Tralli <jtrallii@live.com>
2025-03-22 14:08:50 -0300

    feat: criação da pagina de cadastro de remedios

--- frontend/src/main/java/system/MainApplication.java
+++ frontend/src/main/java/system/MainApplication.java
@@ -8,8 +8,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainApplication extends Application {
+
+    private static Stage paginaInicial;
     @Override
     public void start(Stage stage) throws IOException {
+        paginaInicial = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("pagina-inicial.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 800);
         stage.setTitle("KJSystem");
@@ -20,4 +23,11 @@ public class MainApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+    // Método para acessar o Stage principal em outras classes
+    public static Stage getPaginaInicial() {
+        return paginaInicial;
+    }
+
+
 }
--- frontend/src/main/java/system/controller/PaginaInicialController.java
+++ frontend/src/main/java/system/controller/PaginaInicialController.java
@@ -1,7 +1,16 @@
 package system.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import system.MainApplication;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class PaginaInicialController {
     @FXML
@@ -10,5 +19,37 @@ public class PaginaInicialController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void cadastroRemedios() throws IOException {
+//        // Obtém o Stage atual
+//        Stage stage = MainApplication.getPaginaInicial();
+//
+//        // Carrega o novo FXML
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/system/pagina-cadastro-remedio.fxml")));
+//
+//
+//
+//        // Define a nova cena no Stage atual
+//        stage.setScene(new Scene(root));
+        URL fxmlURL = getClass().getResource("/system/pagina-cadastro-remedio.fxml");
+
+        System.out.println(fxmlURL);  // Isso irá imprimir o caminho do arquivo FXML no console
+
+        // Se o fxmlURL for null, significa que o arquivo não foi encontrado, e o problema está no caminho.
+        if (fxmlURL == null) {
+            System.out.println("O arquivo FXML não foi encontrado.");
+            return;
+        }
+
+        // Obtém o Stage atual
+        Stage stage = MainApplication.getPaginaInicial();
+
+        // Carrega o novo FXML
+        Parent root = FXMLLoader.load(fxmlURL);
+
+        // Define a nova cena no Stage atual
+        stage.setScene(new Scene(root));
     }
 }
